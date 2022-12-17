67f3b172272875585ce07f1ce249f27fe48a392b
Nico Martin <mail@nico.dev>
2022-12-17 10:08:25 +0100

    initial commit

---
+++ dist/assets/app.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("TEST");
+
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
